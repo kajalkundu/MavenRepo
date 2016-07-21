4009a95ee5da167b2c82dea631325f3ba7fc5bd2
kajalk <kajal.kundu@gmail.com>
2016-07-21 17:51:18 +0100

    Modified with new value

--- MavenProj/src/main/java/HelloWorld.java
+++ MavenProj/src/main/java/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		
 		
-		System.out.println("This is a small program");
+		System.out.println("This is a small program ghahsfkhd");
 		
 		System.out.print("\n");
   
